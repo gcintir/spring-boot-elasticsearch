f7fa1ca72e5e54393b3732fb78dd951df5b045ac
gcintir <guraycintir@gmail.com>
2020-01-07 00:13:25 +0300

    index create / delete commands

---
+++ src/main/java/springbootelasticsearch/Book.java
@@ -0,0 +1,19 @@
+package springbootelasticsearch;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+
+    private String id;
+    private String title;
+    private String author;
+    private String releaseDate;
+
+}
